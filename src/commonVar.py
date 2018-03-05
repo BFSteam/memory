4d768b8a260d2f30070ea8d7421ab5a2c239a646
inknos <nicola.sella@outlook.it>
2018-03-05 20:15:00 +0100

    setup MAS

--- src/commonVar.py
+++ src/commonVar.py
@@ -23,7 +23,7 @@ P_s = prop * P_a                      # do not overwrite
 
 dim = 3  # state dimension
 
-memorySize = 1  # memory dimension
+memorySize = 10  # memory dimension
 
 overwrite = True  # sources overwrite old news when creating newer
 
