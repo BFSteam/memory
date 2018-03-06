cf4175961c2f3ab7e0b5318634a03986b0a81d77
inknos <nicola.sella@outlook.it>
2018-03-06 11:30:14 +0100

    clustering diameter

--- src/oActions.py
+++ src/oActions.py
@@ -36,13 +36,13 @@ def do2b(address, cycle):  # ask_one in observerActions.txt
         if not os.path.exists(common.project.replace("src", "log")):
             os.makedirs(common.project.replace("src", "log"))
 
-        path = common.project.replace("src", "log/graph.gml")
-        nx.write_gml(common.G, path, stringizer=worldAgentStringsizer)
-        print("saved", path)
+        #path = common.project.replace("src", "log/graph.gml")
+        #nx.write_gml(common.G, path, stringizer=worldAgentStringsizer)
+        #print("saved", path)
 
-        path = common.project.replace("src", "log/graphN.gml")
-        nx.write_gml(common.G, path, stringizer=singleNewsStringsizer)
-        print("saved", path)
+        #path = common.project.replace("src", "log/graphN.gml")
+        #nx.write_gml(common.G, path, stringizer=singleNewsStringsizer)
+        #print("saved", path)
 
         path = common.project.replace("src", "log/connectionLog.csv")
         common.conlog.writeLog(path=path, write=common.writeConnectons)
@@ -56,9 +56,23 @@ def do2b(address, cycle):  # ask_one in observerActions.txt
         common.memlog.writeLog(path=path, write=common.writeMemories)
         print("saved", path)
 
-        path = common.project.replace("src", "log/grade_distr.dict.csv")        
+        path = common.project.replace("src", "log/degree_distr.csv")        
         w = csv.writer(open(path, "w"))
         w.writerow(["#header"])
         w.writerow(["node", "degree"])
         for key, val in dict(common.G.degree()).items():
             w.writerow([key, val])
+
+        path = common.project.replace("src", "log/clustering.csv")
+        clus = nx.clustering(common.G)
+        w = csv.writer(open(path, "w"))
+        w.writerow(["#header"])
+        w.writerow(["node", "clustering coeff"])
+        for key, val in clus.items():
+            w.writerow([key, val])
+
+        path = common.project.replace("src", "log/diameter.csv")
+        diam = nx.diameter(max(nx.connected_component_subgraphs(common.G), key=len))
+        w = csv.writer(open(path, "w"))
+        w.writerow(["#header"])
+        w.writerow([diam])
